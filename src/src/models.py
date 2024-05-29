7331cddbfed514f6fbd01a5cf872b4007c7a9ff1
Leon <leon.ehrwein@students.hevs.ch>
2024-05-29 16:47:21 +0200

    test comment

--- src/src/models.py
+++ src/src/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Comment(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     body = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.body[:20]}"
--- src/src/views.py
+++ src/src/views.py
@@ -1,34 +1,21 @@
-# src/views.py
 from django.shortcuts import render, redirect
-from django import forms
 from .forms import CommentForm
 from .models import Comment
-
-
 
 def index(request):
     return render(request, 'index.html')
 
-
 def opinions(request):
     return render(request, 'opinions.html')
 
-
 def question_reponses(request):
-    return render(request, 'question_reponses.html')
-
-
-def comment_form(request):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('FORUM.html')
-            #redirige vers une page de confirmation ou une autre vue
+            return redirect('question_reponses')
     else:
         form = CommentForm()
-    return render(request, 'question_reponses.html', {'form': form})
 
-def show_comments(request):
-    comments = Comment.objects.all()
-    return
+    comments = Comment.objects.all().order_by('-created_at')
+    return render(request, 'question_reponses.html', {'form': form, 'comments': comments})
