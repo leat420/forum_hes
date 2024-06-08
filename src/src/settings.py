53bd99deb38f57ecdc5aab68de91b1f2daefef38
leat420 <lea.tonossi@hes-so.ch>
2024-06-08 21:30:53 +0200

    OK
    presque tout bon, sauf:
    -fuseau horaire
    -redirection "répondre"

--- src/src/settings.py
+++ src/src/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fr-CH'
 
-TIME_ZONE = 'UTC+2'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -122,3 +122,6 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEBUG = True
